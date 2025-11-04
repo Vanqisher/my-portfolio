71f54112c71aa439f68b39e306cead087a7deb3e
Marwan Hatem <113316914+Vanqisher@users.noreply.github.com>
2025-11-05 00:25:05 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MarwanPortfolio from './MarwanPortfolio.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MarwanPortfolio />
-  </React.StrictMode>
-);
